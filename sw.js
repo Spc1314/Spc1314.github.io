ffcd0d05c084e1362b09212935ff38957ff7f3f3
Spc1314 <767548173@qq.com>
2026-02-18 17:08:57 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvic.com",
-    "zoneId": 10612102
+    "domain": "3nbf4.com",
+    "zoneId": 10623999
 }
 self.lary = ""
-importScripts("https://5gvic.com/act/files/service-worker.min.js?r=sw")
+importScripts("https://3nbf4.com/act/files/service-worker.min.js?r=sw")
